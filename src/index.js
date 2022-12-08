6d377229cb2a2dc318b9b14b2f79a9b484e208d6
gthomas513 <gabe.thomas513@gmail.com>
2022-12-08 11:06:51 -0500

    added Roboto font + more styling

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ class App extends React.Component {
         height={"120px"}
         />
       <h1>Application for Onboarding</h1>
+      <h3>
+        <p>Note: Please make sure all fields are accurately filled out.</p>
+
+        <p>You may reference the inline text within each field to see how it would be written.</p>
+
+        <p>All information should match your governemt issued identification.</p>
+      </h3>
       <Form />
     </div>)
   }
